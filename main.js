2c22f99a07d54e9794f10bf4002fab778423841b
BernardDev <bernard.devworks@gmail.com>
2021-01-06 18:12:37 +0100

    minify and refactor js

--- main.js
+++ main.js
@@ -1,99 +1,139 @@
 function executeTest() {
-  function eventListening() {
-    //
-    const button = document.getElementById('fetchBtn');
-
-    button.addEventListener('click', () => {
-      const input = document.getElementById('inputAPI');
-
-      const value = input.value;
-
-      getApiJSON(value);
-    });
+  //
+  function addEventListenerFetch() {
+    document
+      .getElementById('btn--fetch')
+      .addEventListener('click', fetchJSONBasedOnUserInput);
   }
 
-  async function getApiJSON(value) {
-    // GET RESPONSE
-    let res = await fetch(`${value}`);
-    // ---------------------------------
-
-    // RESET STATUS
-    const status = document.querySelector('.explorerStatus');
-    status.innerText = '';
-    // ---------------------------------
-
-    // STYLING STATUS MESSAGE : EXPLORER
-    let code = res.ok ? 'Ok' : 'Not ok';
-    if (res.ok) {
-      document.querySelector('.explorerStatus').style.backgroundColor =
-        '#28ff85';
-    } else {
-      // console.log(status, 'status');
-      document.querySelector('.explorerStatus').style.backgroundColor =
-        '#ff4128';
-    }
-
-    const statuscode = document.createTextNode(
-      `Response: ${res.status} ${code}`
-    );
-
-    status.appendChild(statuscode);
-    // ---------------------------------
-
-    // INSERTING TEMPLATE
-    let temp1 = document.querySelector('.template').content;
-    let temp1copy = temp1.cloneNode(true);
-
-    // ---------------------------------
-    // STYLING STATUS MESSAGE : CARD
-
-    const statuscodeCard = document.createTextNode(
-      `Response: ${res.status} ${code}`
-    );
-
-    temp1copy.querySelector('.cardStatus').appendChild(statuscodeCard);
-    // ---------------------------------
-
-    // INSERTING API URL
-    const APIurl = temp1copy.querySelector('.APIurl');
-
-    const APIvalue = document.createTextNode(value);
-
-    APIurl.appendChild(APIvalue);
-    // ---------------------------------
-
-    // STYLING STATUS MESSAGE : CARD 2
-    if (res.ok) {
-      temp1copy.querySelector('.cardStatus').style.backgroundColor = '#28ff85';
-    } else {
-      temp1copy.querySelector('.cardStatus').style.backgroundColor = '#ff4128';
-    }
-    // ---------------------------------
-
-    // INSERTING TEMPLATE 2
-    document.querySelector('.left').appendChild(temp1copy);
-    // ---------------------------------
-
-    // HANDELING JSON RESPONSE
-    if (res.ok) {
-      let data = await res.json();
-
-      let jsonParsed = JSON.stringify(data, null, 4);
-
-      const pre = document.createElement('pre');
-
-      const API = document.getElementById('resAPI');
-
-      resAPI.appendChild(pre);
-
-      const text = document.createTextNode(jsonParsed);
-
-      pre.appendChild(text);
-    } else {
-      document.getElementById('resAPI').innerHTML = '';
-    }
-    // ---------------------------------
+  function getURLfromInput() {
+    return document.getElementById('inputField').value;
   }
-  eventListening();
+
+  function fetchJSONBasedOnUserInput() {
+    const url = getURLfromInput();
+    delegateNfetch(url);
+  }
+
+  async function delegateNfetch(url) {
+    const res = await fetchJSON(url);
+    addToHistory(res, url);
+    displayInExplorer(res);
+  }
+
+  async function fetchJSON(url) {
+    try {
+      let res = await fetch(`${url}`);
+      if (res.ok) {
+        return {status: res.status, ok: res.ok, json: await res.json()};
+      } else {
+        return {status: res.status, ok: res.ok};
+      }
+    } catch (e) {
+      console.log(e);
+      return {status: 'error', ok: false};
+    }
+  }
+
+  function createStatus(status, code) {
+    const statusMessage = document.createTextNode(
+      `Response: ${status} ${code}`
+    );
+    return statusMessage;
+  }
+
+  function displayJson(json) {
+    let jsonParsed = JSON.stringify(json, null, 4);
+    const pre = document.createElement('pre');
+    const text = document.createTextNode(jsonParsed);
+    pre.appendChild(text);
+    document.getElementById('displayedJson').appendChild(pre);
+  }
+
+  function displayInExplorer(res) {
+    document.getElementById('displayedJson').innerHTML = '';
+    if (res.ok) {
+      displayJson(res.json);
+    }
+    const explorerStatus = document.querySelector('.explorerStatus');
+    explorerStatus.innerText = '';
+
+    displayStatus(res, explorerStatus);
+  }
+
+  function addToHistory(res, url) {
+    let historyCard = createHistoryCard(res, url);
+    document.querySelector('.templateRender').appendChild(historyCard);
+  }
+
+  function createHistoryCard(res, url) {
+    let historyTemplate = document.querySelector('.template').content;
+    let historyCard = historyTemplate.cloneNode(true);
+
+    const templateStatus = historyCard.querySelector('.historyStatus');
+    displayStatus(res, templateStatus);
+    const targetUrl = document.createTextNode(url);
+    historyCard.querySelector('.url').appendChild(targetUrl);
+    return historyCard;
+  }
+
+  function displayStatus(res, element) {
+    let code;
+    if (res.ok) {
+      code = 'Ok';
+      element.style.backgroundColor = '#28ff85';
+    } else {
+      code = 'Not ok';
+      element.style.backgroundColor = '#ff4128';
+    }
+
+    const statusMessage = createStatus(res.status, code);
+    element.appendChild(statusMessage);
+  }
+  addEventListenerFetch();
 }
 executeTest();
+
+// ------------------
+// display data explorer: res{}
+// display data in history: res{}
+
+// similar?
+// different yes /no? parameters
+
+// functionality
+// chunking
+// data vs formatting
+// isolate test
+// naming: describing
+// delegating function
+
+// const templateRender = document.querySelector('.template');
+// templateRender.style.display = 'block';
+// document.querySelector('#clearBtn').style.display = 'block';
+
+// ---------------------------------
+
+// function eventClear() {
+//   const buttonClear = document.getElementById('clearBtn');
+//   buttonClear.addEventListener('click', () => {
+//     const templateRender = document.querySelector('.template');
+//     console.log(templateRender, 'test');
+//     templateRender.style.display = 'none';
+//   });
+// }
+
+// return {
+//   status: (res.status = status),
+//   ok: (res.ok = statusOk),
+//   json: (await res.json() = resJson),
+// };
+
+// nieuwe functie
+// stukje code daar in plakken
+// parameters (placeholders) invullen
+// content afhankelijk van parameter herschrijven
+// returnen ?
+// bedenken waar de functie te callen
+// arguments invullen
+//
